d008d0369faa080bec468b8ba45bf8f0e1d66fce
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-20 19:20:57 +0300

    modify title color schemes in signup.xml

--- app/src/main/java/com/moringaschool/kenyatravelguide/SignupActivity.java
+++ app/src/main/java/com/moringaschool/kenyatravelguide/SignupActivity.java
@@ -2,16 +2,22 @@ package com.moringaschool.kenyatravelguide;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
     private Button mSignUpButton;
+    private TextView mWelcomeMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+        mWelcomeMessage = findViewById(R.id.welcomeMessage);
         mSignUpButton = (Button)findViewById(R.id.signUp);
+        String title = "<font color=#F3A333>WELCOME TO</font> <font color=#F16821>KENYA TRAVEL GUIDE</font>";
+        mWelcomeMessage.setText(Html.fromHtml(title));
     }
 }
