623fea6cd95c7f1e59b698b262769854c44494ab
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-20 18:40:56 +0300

    add font awesome icons

--- app/src/main/java/com/moringaschool/kenyatravelguide/LoginActivity.java
+++ app/src/main/java/com/moringaschool/kenyatravelguide/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.moringaschool.kenyatravelguide;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
