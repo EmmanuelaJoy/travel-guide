4288453c12f0a773f1f329561ef9dd23f6f7a715
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-20 19:27:53 +0300

    add an intent to log in link in signup.xml

--- app/src/main/java/com/moringaschool/kenyatravelguide/SignupActivity.java
+++ app/src/main/java/com/moringaschool/kenyatravelguide/SignupActivity.java
@@ -3,6 +3,7 @@ package com.moringaschool.kenyatravelguide;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,13 +12,24 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SignupActivity extends AppCompatActivity {
     private Button mSignUpButton;
     private TextView mWelcomeMessage;
+    private TextView mLogInMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
         mWelcomeMessage = findViewById(R.id.welcomeMessage);
         mSignUpButton = (Button)findViewById(R.id.signUp);
+        mLogInMessage = (TextView)findViewById(R.id.loginLink);
+        mLogInMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
         String title = "<font color=#F3A333>WELCOME TO</font> <font color=#F16821>KENYA TRAVEL GUIDE</font>";
+        String text = "<font color=#212121>ALready have an account?</font> <font color=#F16821>Log In</font>";
         mWelcomeMessage.setText(Html.fromHtml(title));
+        mLogInMessage.setText(Html.fromHtml(text));
     }
 }
