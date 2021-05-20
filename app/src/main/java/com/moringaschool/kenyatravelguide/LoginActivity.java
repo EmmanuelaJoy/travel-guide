a732e21add4b67ac23375aea8a97d492472a718f
Emmanuela Joy <emmajoy81@gmail.com>
2021-05-20 18:56:45 +0300

    modify title color schemes in login.xml

--- app/src/main/java/com/moringaschool/kenyatravelguide/LoginActivity.java
+++ app/src/main/java/com/moringaschool/kenyatravelguide/LoginActivity.java
@@ -3,18 +3,22 @@ package com.moringaschool.kenyatravelguide;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     private Button mLoginButton;
+    private TextView mWelcomeMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        mWelcomeMessage = findViewById(R.id.welcomeMessage);
         mLoginButton = (Button)findViewById(R.id.login);
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +26,7 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+        String text = "<font color=#F3A333>Welcome To</font> <font color=#F16821>Kenya Travel Guide</font>";
+        mWelcomeMessage.setText(Html.fromHtml(text));
     }
 }
