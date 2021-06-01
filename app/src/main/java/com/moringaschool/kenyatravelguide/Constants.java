353c6e18fd4547fe7913ee931e68d453df407268
Emmanuela Joy <emmajoy81@gmail.com>
2021-06-01 18:15:19 +0300

    add Open Trip api

---
+++ app/src/main/java/com/moringaschool/kenyatravelguide/Constants.java
@@ -0,0 +1,6 @@
+package com.moringaschool.kenyatravelguide;
+
+public class Constants {
+    public static final String OPENTRIPMAP_BASE_URL = "http://api.opentripmap.com/";
+    public static final String OPENTRIPMAP_API_KEY = BuildConfig.OPENTRIPMAP_API_KEY;
+}
