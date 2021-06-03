4f7948839b546f1825d199b5e1706f0b56162b82
Emmanuela Joy <emmajoy81@gmail.com>
2021-06-03 13:45:12 +0300

    add a signout method to logout item in navigation drawer

--- app/src/main/java/com/moringaschool/kenyatravelguide/DashboardActivity.java
+++ app/src/main/java/com/moringaschool/kenyatravelguide/DashboardActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 
@@ -17,6 +22,7 @@ public class DashboardActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    TextView logout;
 
 //    @BindView(R.id.drawerlayout) DrawerLayout drawerLayout;
 //    @BindView(R.id.username) NavigationView navMenu;
@@ -29,12 +35,22 @@ public class DashboardActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawerlayout);
         navigationView = findViewById(R.id.navMenu);
         toolbar = findViewById(R.id.toolbar);
+        logout = findViewById(R.id.nav_logout);
 
         setSupportActionBar(toolbar);
 
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
