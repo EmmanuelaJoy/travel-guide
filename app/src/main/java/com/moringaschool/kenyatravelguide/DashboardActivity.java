e65ed83742efb3fea3865b6f28e7d013fa4c1b7f
Emmanuela Joy <emmajoy81@gmail.com>
2021-06-03 13:57:33 +0300

    set the display name and email in navigaton drawer to signed in user's name and email

--- app/src/main/java/com/moringaschool/kenyatravelguide/DashboardActivity.java
+++ app/src/main/java/com/moringaschool/kenyatravelguide/DashboardActivity.java
@@ -9,8 +9,11 @@ import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,30 +21,31 @@ import com.google.firebase.auth.FirebaseUser;
 import butterknife.BindView;
 
 public class DashboardActivity extends AppCompatActivity {
-
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    Toolbar toolbar;
-    TextView logout;
-
-//    @BindView(R.id.drawerlayout) DrawerLayout drawerLayout;
-//    @BindView(R.id.username) NavigationView navMenu;
-//    @BindView(R.id.password) Toolbar toolbar;
+    @BindView(R.id.drawerlayout) DrawerLayout drawerLayout;
+    @BindView(R.id.username) NavigationView navMenu;
+    @BindView(R.id.password) Toolbar toolbar;
+    @BindView(R.id.nav_logout) TextView logout;
+    @BindView(R.id.userName) TextView signedInUserName;
+    @BindView(R.id.userEmail) TextView signedInUserEmail;
+    @BindView(R.id.userProfileImage) ImageView signedInUserProfileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        drawerLayout = findViewById(R.id.drawerlayout);
-        navigationView = findViewById(R.id.navMenu);
-        toolbar = findViewById(R.id.toolbar);
-        logout = findViewById(R.id.nav_logout);
 
         setSupportActionBar(toolbar);
 
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(signInAccount != null){
+            signedInUserProfileImage.setImageURI(signInAccount.getPhotoUrl());
+            signedInUserName.setText(signInAccount.getDisplayName());
+            signedInUserEmail.setText(signInAccount.getEmail());
+        }
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
