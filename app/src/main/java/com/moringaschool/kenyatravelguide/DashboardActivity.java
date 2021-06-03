968be2ca843e14381a6a6aae4abcc05f2d319db9
Emmanuela Joy <emmajoy81@gmail.com>
2021-06-03 13:24:30 +0300

    add a custom toolbar and navigation drawer

--- app/src/main/java/com/moringaschool/kenyatravelguide/DashboardActivity.java
+++ app/src/main/java/com/moringaschool/kenyatravelguide/DashboardActivity.java
@@ -1,14 +1,41 @@
 package com.moringaschool.kenyatravelguide;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import butterknife.BindView;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+//    @BindView(R.id.drawerlayout) DrawerLayout drawerLayout;
+//    @BindView(R.id.username) NavigationView navMenu;
+//    @BindView(R.id.password) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        drawerLayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navMenu);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
     }
+
 }
